0242c48203df8893221a92d7770563deef98145c
Psycholab.ljz <psyljz@gmail.com>
2024-07-15 19:23:47 +0800

    go1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { ConnectButton } from "./layout";
 export default function Home() {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline"></h1>
+
+			<h1 className="text-3xl font-bold underline">Simple Test to connect wallet openspace</h1>
 			<ConnectButton />
 		</div>
 	)
